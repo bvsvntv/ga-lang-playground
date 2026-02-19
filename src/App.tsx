87231e0571eb56a5d395a4594778a4d99ed4c9d2
bvsvntv <developer.basanta@gmail.com>
2026-02-19 13:56:18 +0545

    Detect modifier key press

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [downKey, setDownKey] = useState(0);
+  const [downKey, setDownKey] = useState<number>(0);
+  const [downKeyCode, setDownKeyCode] = useState<string>("Key{X}");
+
+  const [altKey, setAltKey] = useState<boolean>(false);
+  const [ctrlKey, setCtrlKey] = useState<boolean>(false);
+  const [shiftKey, setShiftKey] = useState<boolean>(false);
 
   function handleOnKeyUp(e: React.KeyboardEvent) {
-    setDownKey(+e.keyCode);
+      if(e.altKey) setAltKey(true);
+      else if(e.shiftKey) setShiftKey(true);
+      else if(e.ctrlKey) setCtrlKey(true);
+      else {
+          setDownKey(+e.keyCode)
+          setDownKeyCode(e.code)
+      };
   }
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
       <main className="min-h-screen w-full max-w-3xl px-4 py-16">
         <h3 className="text-md font-semibold text-zinc-700">
-          PRESSED: {downKey}
+          PRESSED: {downKey}/{downKeyCode} | ALT_KEY: {altKey ? "YES" : "NO"} | CTRL_KEY: {ctrlKey ? "YES" : "NO"} | SHIFT_KEY: {shiftKey ? "YES" : "NO"}
         </h3>
 
         <textarea
