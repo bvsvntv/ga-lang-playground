e292ed8c80f7efea5329cb204c37836fcc67e87f
bvsvntv <developer.basanta@gmail.com>
2026-02-19 11:59:01 +0545

    Capture and display currently pressed key's key code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import React, { useState } from 'react';
+
 function App() {
+  const [downKey, setDownKey] = useState(0);
+
+  function handleOnKeyUp(e: React.KeyboardEvent) {
+    setDownKey(+e.keyCode);
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between px-4 py-16">
+      <main className="min-h-screen w-full max-w-3xl px-4 py-16">
+        <h3 className="text-md font-semibold text-zinc-700">
+          PRESSED: {downKey}
+        </h3>
+
         <textarea
+          onKeyDown={(e) => handleOnKeyUp(e)}
           rows={8}
-          className="focus:border-1 w-full rounded border border-gray-400 p-2 text-black focus:border-gray-500 focus:outline-none"
+          className="focus:border-1 text-zinc mt-2 w-full rounded border border-zinc-400 p-2 focus:border-zinc-500 focus:outline-none"
         />
       </main>
     </div>
