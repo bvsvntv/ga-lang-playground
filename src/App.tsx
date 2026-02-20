9625b3805ab36972f290f207b3bed43090271741
bvsvntv <developer.basanta@gmail.com>
2026-02-20 16:14:05 +0545

    Create fields to take input and show output contents

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,51 @@
-import React, { useState } from 'react';
-
-type LastKeyDetail = {
-  key: string;
-  keyCode: number;
-  altKey: boolean;
-  ctrlKey: boolean;
-  shiftKey: boolean;
-};
+import { useState } from 'react';
 
 function App() {
-  const [lastKeyDetail, setLastKeyDetail] = useState<LastKeyDetail>({
-    key: '',
-    keyCode: 0,
-    altKey: false,
-    ctrlKey: false,
-    shiftKey: false,
-  });
+  const [input, setInput] = useState<string>('');
+  const [output, setOutput] = useState<string>('');
 
-  function handleKeyDown(e: React.KeyboardEvent) {
-    setLastKeyDetail({
-      key: e.key,
-      keyCode: e.keyCode,
-      altKey: e.altKey,
-      ctrlKey: e.ctrlKey,
-      shiftKey: e.shiftKey,
-    });
+  function handleRun() {
+    setOutput(input);
+  }
+
+  function handleReset() {
+    setInput('');
+    setOutput('');
   }
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
       <main className="min-h-screen w-full max-w-3xl px-4 py-16">
-        <h3 className="text-md font-semibold text-zinc-700">
-          PRESSED: {lastKeyDetail.keyCode}/{lastKeyDetail.key} | ALT_KEY:{' '}
-          {lastKeyDetail.altKey ? 'YES' : 'NO'} | CTRL_KEY:{' '}
-          {lastKeyDetail.ctrlKey ? 'YES' : 'NO'} | SHIFT_KEY:{' '}
-          {lastKeyDetail.shiftKey ? 'YES' : 'NO'}
-        </h3>
+        <section className="flex justify-between gap-4">
+          <textarea
+            rows={8}
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="focus:border-1 text-zinc w-full rounded border border-zinc-400 p-2 focus:border-zinc-500 focus:outline-none"
+          />
+          <textarea
+            rows={8}
+            disabled
+            value={output}
+            className="text-zinc w-full resize-none rounded border border-none border-zinc-400 bg-zinc-200 p-2 focus:border-zinc-500 focus:outline-none"
+          />
+        </section>
 
-        <textarea
-          onKeyDown={handleKeyDown}
-          rows={8}
-          className="focus:border-1 text-zinc mt-2 w-full rounded border border-zinc-400 p-2 focus:border-zinc-500 focus:outline-none"
-        />
+        <div className="mt-2 flex gap-2">
+          <button
+            className="rounded bg-green-500 px-4 py-2 text-xl font-semibold text-white"
+            onClick={handleRun}
+          >
+            Run
+          </button>
+
+          <button
+            className="rounded border border-zinc-400 bg-zinc-50 px-4 py-2 text-xl font-semibold text-zinc-700"
+            onClick={handleReset}
+          >
+            Reset
+          </button>
+        </div>
       </main>
     </div>
   );
