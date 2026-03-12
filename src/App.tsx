2acb413a78ce48bfec86bc7a849f06872cc0a168
bvsvntv <developer.basanta@gmail.com>
2026-03-12 17:59:53 +0545

    Add clear button

--- src/App.tsx
+++ src/App.tsx
@@ -32,29 +32,34 @@ function App() {
   }
 
   function handleReset() {
+    setInput(message);
+    setOutput('');
+  }
+
+  function handleClear() {
     setInput('');
     setOutput('');
   }
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
-      <main className="min-h-screen w-full max-w-3xl px-4 py-16">
+      <main className="min-h-screen w-full max-w-7xl px-4 py-16">
         <section className="flex justify-between gap-4">
           <textarea
-            rows={8}
+            rows={16}
             value={input}
             onChange={(e) => setInput(e.target.value)}
             className="focus:border-1 text-zinc w-full rounded border border-zinc-400 p-2 focus:border-zinc-500 focus:outline-none"
           />
           <textarea
-            rows={8}
+            rows={16}
             disabled
             value={output}
             className="text-zinc w-full resize-none rounded border border-none border-zinc-400 bg-zinc-200 p-2 focus:border-zinc-500 focus:outline-none"
           />
         </section>
 
-        <div className="mt-2 flex gap-2">
+        <section className="mt-4 flex gap-2">
           <button
             className="rounded bg-green-500 px-4 py-2 text-xl font-semibold text-white"
             onClick={handleRun}
@@ -68,7 +73,14 @@ function App() {
           >
             Reset
           </button>
-        </div>
+
+          <button
+            className="rounded border border-zinc-400 bg-red-100 px-4 py-2 text-xl font-semibold text-zinc-700"
+            onClick={handleClear}
+          >
+            Clear
+          </button>
+        </section>
       </main>
     </div>
   );
