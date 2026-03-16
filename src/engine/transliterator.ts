e7ac47a6f66c5586e224a8599f60fbc74e265c9b
bvsvntv <developer.basanta@gmail.com>
2026-03-16 10:02:20 +0545

    Extract API preferences into constants

--- src/engine/transliterator.ts
+++ src/engine/transliterator.ts
@@ -1,12 +1,24 @@
 export async function transliterate(text: string): Promise<string[]> {
-  const url = `https://inputtools.google.com/request?text=${encodeURIComponent(text)}&itc=ne-t-i0-und&num=3`;
+  // Constants for API
+  const PREFERRED_LANGUAGE_CODE = 'ne-t-i0-und'; // Nepali language
+  const PREFERRED_MAX_RESULT = 3;
+
+  const url = `https://inputtools.google.com/request?text=${encodeURIComponent(text)}&itc=${PREFERRED_LANGUAGE_CODE}&num=${PREFERRED_MAX_RESULT}`;
 
   const response = await fetch(url);
   if (!response.ok) {
     console.log('Google input error. ERROR: ', response.status);
+    return [];
   }
 
-  const parsed = await response.json();
-  const suggestions: string[] = parsed[1][0][1];
+  const serialized = await response.json();
+  if (
+    typeof serialized[1] === 'undefined' ||
+    typeof serialized[1][0] === 'undefined'
+  ) {
+    return [];
+  }
+
+  const suggestions: string[] = serialized[1][0][1];
   return suggestions ?? text;
 }
