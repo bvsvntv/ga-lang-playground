a21122d7e816f4a1653a5e43c3402fbd2b3a2f76
bvsvntv <developer.basanta@gmail.com>
2026-04-02 08:26:28 +0545

    Fix ps1 character spacing

--- src/components/console.tsx
+++ src/components/console.tsx
@@ -5,10 +5,9 @@ type ConsoleProps = {
 export function Console({ output }: ConsoleProps) {
   return (
     <pre className="w-full resize-none rounded border border-none border-zinc-400 bg-zinc-200 p-2 text-zinc-500">
-      {' '}
       <span className="text-lg font-bold text-green-500">
-        {' '}
-        {'\u0917'} {'\u00B7'}λ{'>'}{' '}
+        {'\u0917'}
+        {'\u00B7'}λ{'>'}{' '}
       </span>
       {output}
     </pre>
