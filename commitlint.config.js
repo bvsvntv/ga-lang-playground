c8d30962128a661a19538256aafb7783009ca68e
bvsvntv <developer.basanta@gmail.com>
2026-07-15 16:20:58 +0545

    Integrate lint-staged with husky pre-commit hook and add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+/** @type {import('@commitlint/types').UserConfig} */
+export default {
+  rules: {
+    'scope-case': [2, 'always', 'sentence-case'],
+    'subject-max-length': [2, 'always', 72],
+  },
+};
